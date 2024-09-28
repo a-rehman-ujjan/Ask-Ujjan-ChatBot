b4435c0e82b83342d9cf83612112b1ac51a9739d
Abdul Rehman Ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2024-09-29 03:41:13 +0500

    Update settings.py

--- Backend (Python)/flash_chatbot/settings.py
+++ Backend (Python)/flash_chatbot/settings.py
@@ -7,13 +7,4 @@ try:
 except FileNotFoundError:
     config = Config()
 
-# BOOTSTRAP_SERVER = config("KAFKA_SERVER", cast=str)
-# KAFKA_ORDER_TOPIC = config("KAFKA_ORDER_TOPIC", cast=str)
-# KAFKA_CONSUMER_GROUP_ID = config("KAFKA_CONSUMER_GROUP_ID", cast=str)
-# DATABASE_URL=config("DATABASE_URL", cast= Secret)
-# TEST_DATABASE_URL=config("TEST_DATABASE_URL",  cast= Secret)
-# ADMIN_PASSWORD=config("ADMIN_PASSWORD", cast= Secret)
-# SECRET_KEY = config("SECRET_KEY", cast=Secret)
-# ALGORITHM = config("ALGORITHM", cast=str)
-
 GEMINI_API_KEY = config("GEMINI_API_KEY", cast=Secret)
